b255c779122304b42a8eeac82a7d53cfc21b5ffc
Runtić Ivan <runta_osk@hotmail.com>
2019-02-22 17:40:22 +0100

    zbroj_razlika

---
+++ ConsoleApp1/temperatura/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace temperatura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ledište vode 0°C");
+            Console.WriteLine("vrelište vode 100°C");
+
+            Console.ReadLine();
+
+        }
+    }
+}
