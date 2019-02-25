7d2a8c7671d2fae30c2f2aeb3d57660ca6d5f508
Runta90 <ivan.runtic90@gmail.com>
2019-02-25 17:17:30 +0100

    switch

---
+++ ConsoleApp1/5.2.2_switch/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._2._2_switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a, b = 0;
+                string operacija = "";
+
+            Console.WriteLine("Unesite 1. prirodan broj: ");
+             a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Unesite 2. prirodni broj: ");
+             b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Unesite operaciju (+,-,*,/,): ");
+            operacija = Console.ReadLine();
+
+
+
+            switch (operacija)
+            {
+                case "+":Console.WriteLine("Zbroj je {0} + {1} = {2}", a, b, (a + b));
+                    break;
+
+                case "-":
+                    Console.WriteLine("Razlika je {0} - {1} = {2}", a, b, (a - b));
+                    break;
+
+                case "*":
+                    Console.WriteLine("Umnožak je {0} * {1} = {2}", a, b, (a * b));
+                    break;
+
+                case "/":
+                    Console.WriteLine("Kvocijent je {0} / {1} = {2}", a, b, (a / b));
+                    break;
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
