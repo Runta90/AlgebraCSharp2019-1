a9c59183f21c1b7a721dda6ff36ef111e16e7031
Runta90 <ivan.runtic90@gmail.com>
2019-02-25 16:58:28 +0100

    5.2.1.2

---
+++ ConsoleApp1/5.2.1.2_pozitivan/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._2._1._2_pozitivan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite cijeli broj: ");
+            int a = int.Parse(Console.ReadLine());
+
+            if (a > 0)
+            {
+                Console.WriteLine("Uneseni broj je pozivan");
+            }
+
+            else
+            {
+                if (a < 0)
+                {
+                    Console.WriteLine("Uneseni broj je negativan");
+                }
+
+                else
+                {
+                    Console.WriteLine("Uneseni broj je jednak 0");
+                }
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
