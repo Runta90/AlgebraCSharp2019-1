6d38d6592c602fcfaa5cd3369b876b7f0fe33564
Runta90 <ivan.runtic90@gmail.com>
2019-02-25 17:54:30 +0100

    exception

---
+++ ConsoleApp1/5.2.3_exception/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._2._3_exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+            
+        {
+            try
+            {
+
+                Console.WriteLine("Unesite cijeli broj: ");
+                int a = int.Parse(Console.ReadLine());
+
+                if (a > 0)
+                {
+                    Console.WriteLine("Uneseni broj je pozivan");
+                }
+
+                else
+                {
+                    if (a < 0)
+                    {
+                        Console.WriteLine("Uneseni broj je negativan");
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Uneseni broj je jednak 0");
+                    }
+                }
+            }
+
+
+
+
+            catch (FormatException fex)
+            {
+                Console.WriteLine("GREŠKA, pogrešan format:" + fex.Message);
+
+
+            }
+        
+
+            Console.ReadKey();
+
+        }
+    }
+}
