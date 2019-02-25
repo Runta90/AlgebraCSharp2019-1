077062e597dcadc578040b496c8e1f635eed1717
Runta90 <ivan.runtic90@gmail.com>
2019-02-25 16:36:29 +0100

    uvjetno granjanje

---
+++ ConsoleApp1/pozitivan_i_negativan_broj/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pozitivan_i_negativan_broj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite broj: ");
+            int broj = int.Parse(Console.ReadLine());
+
+            if (broj > 0)
+            {
+                Console.Writeline("Broj je pozitivan");
+            }
+
+            else
+            {
+                if (broj == 0)
+
+                {
+                    Console.WriteLine("Broje je nula!!");
+                }
+                else
+                {
+                    Console.WriteLine("Broj je negativan");
+                }
+
+            }
+
+            Console.ReadKey();
+            
+        
+        }
+    }
+}
