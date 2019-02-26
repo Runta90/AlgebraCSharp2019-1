9078da41afdf38bf2ae90678ec9e6cc21ba73dba
Runta90 <ivan.runtic90@gmail.com>
2019-02-26 16:43:16 +0100

    dobar broj 1

---
+++ ConsoleApp1/zadatak_5.2.5_u_intervalu/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak_5._2._5_u_intervalu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite cijeli broj:");
+
+            int a = int.Parse(Console.ReadLine());
+
+            if (a >= 100 && a <= 200)
+            {
+                Console.Write("Broj je u intervalu [100, 200]");
+            }
+
+            else
+            {
+                Console.Write("Broj nije u intervalu [100, 200]");
+            }
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
---
+++ ConsoleApp1/zadatak_5.2.6_dobar_broj_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak_5._2._6_dobar_broj_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite cijeli broj:");
+
+            int a = int.Parse(Console.ReadLine());
+
+            if (a % 5 == 0 || a % 3 == 0)
+            {
+                Console.Write("Broj je dobar");
+            }
+
+            else
+            {
+                Console.Write("Broj nije dobar");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
