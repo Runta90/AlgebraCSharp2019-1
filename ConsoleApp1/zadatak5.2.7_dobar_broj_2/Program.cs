e46d25753db9098e7ca7c9e303ca23318ba03818
Runta90 <ivan.runtic90@gmail.com>
2019-02-26 16:47:43 +0100

    dobar broj 2

---
+++ ConsoleApp1/zadatak5.2.7_dobar_broj_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak5._2._7_dobar_broj_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite cijeli broj:");
+
+            int a = int.Parse(Console.ReadLine());
+
+            if (a % 4 == 0 || a >= 100)
+            {
+                Console.Write("Broj je dobar");
+            }
+
+            else
+            {
+                Console.Write("Broj nije dobar");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
