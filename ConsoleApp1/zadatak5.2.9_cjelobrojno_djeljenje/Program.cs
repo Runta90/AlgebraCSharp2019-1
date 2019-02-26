531fb8f97ee20e7cecfe7942f7849c649675d4f1
Runta90 <ivan.runtic90@gmail.com>
2019-02-26 17:21:19 +0100

    cjelobrojno djeljenje

---
+++ ConsoleApp1/zadatak5.2.9_cjelobrojno_djeljenje/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak5._2._9_cjelobrojno_djeljenje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite cijeli broj:");
+            int broj = 0;
+            try
+
+            {
+                broj = int.Parse(Console.ReadLine());
+                if (broj % 2 == 0)
+                {
+                    Console.WriteLine("Broj je paran");
+                }
+
+                if (broj % 3 == 0)
+                {
+                    Console.WriteLine("Broj je djeljiv sa 3");
+                }
+
+                else
+                {
+                    Console.WriteLine("Broj {0} nije djeljiv sa 3, ostatak je {1}", broj, (broj % 3))
+                }
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Provjeri je li unešen broj?" + e.ToString());
+            }
+
+            
+
+
+            Console.ReadKey();
+        }
+    }
+}
