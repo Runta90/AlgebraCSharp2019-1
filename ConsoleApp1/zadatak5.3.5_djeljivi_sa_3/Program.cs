c6baa6326bdab69c1cfe8e1798a99192f091bb4f
Runta90 <ivan.runtic90@gmail.com>
2019-02-27 17:18:22 +0100

    djeljivi sa 3

---
+++ ConsoleApp1/zadatak5.3.5_djeljivi_sa_3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak5._3._5_djeljivi_sa_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+
+         
+        {
+            Console.WriteLine("Unesi prvi broj intervala:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Unesi prvi drugi intervala:");
+            int b = int.Parse(Console.ReadLine());
+
+            for (int i = a; i < b; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine("{0} ", i);
+                }
+            }
+
+            Console.ReadKey();
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
