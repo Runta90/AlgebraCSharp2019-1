90e1649cde49afc487a47425c635556927ccd274
Runta90 <ivan.runtic90@gmail.com>
2019-02-27 17:37:33 +0100

    zadatak 5.3.9 djeljitelji

---
+++ ConsoleApp1/zadatak5.3.9_djeljitelji/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak5._3._9_djeljitelji
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesi prirodan broj");
+            int broj = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Djeljitelji broja {0} su:", broj);
+
+            for (int i = 1; i < broj; i++)
+            {
+                if (broj % i == 0)
+                {
+                    Console.WriteLine("{0}", i);
+                }
+            }
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
