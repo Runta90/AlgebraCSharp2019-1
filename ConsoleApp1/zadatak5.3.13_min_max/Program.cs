19ebaeace7dfd893bc45bdbeea8caaef57f1efa3
Runta90 <ivan.runtic90@gmail.com>
2019-02-27 18:29:15 +0100

    zadatak5.3.13 min_max

---
+++ ConsoleApp1/zadatak5.3.13_min_max/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak5._3._13_min_max
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int min = int.MaxValue;
+            int max = int.MinValue;
+            int broj;
+
+            for (int i = 1; i < 10; i++)
+            {
+                Console.Write("Unesite {0}. prirodni broj: ", i.ToString());
+                broj = int.Parse(Console.ReadLine());
+
+                if (broj < min)
+                {
+                    min = broj;
+                }
+                if (broj > max)
+                {
+                    max = broj;
+                }
+            }
+
+            Console.Write("Najmanji = {0} \nNajveci = {1}", min, max);
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
