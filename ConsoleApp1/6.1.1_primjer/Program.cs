f7d354530d2321c8b49cdc9802433e7ac4f8e968
Runta90 <ivan.runtic90@gmail.com>
2019-03-04 17:28:05 +0100

    6.1.1 primjer

---
+++ ConsoleApp1/6.1.1_primjer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._1._1_primjer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // deklaracija niza od 3 elementa
+            int[] niz = new int[3];
+
+            //init
+            niz[0] = 55;
+            niz[2] = 77;
+            niz[1] = 22;
+
+            for (int i = 0; i < niz.Length; i++)
+            {
+                Console.WriteLine((i+1) + ": " + niz[i]);
+            }
+
+            Console.ReadKey();
+
+ 
+
+
+        }
+    }
+}
