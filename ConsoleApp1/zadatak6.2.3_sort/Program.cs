86a17ff92e9f194fe70577fb29a50cabbb135566
Runta90 <ivan.runtic90@gmail.com>
2019-03-05 17:17:43 +0100

    zadatak 6.2.3 sort

---
+++ ConsoleApp1/zadatak6.2.3_sort/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak6._2._3_sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int broj = -1;
+            ArrayList arr = new ArrayList();
+
+            Console.WriteLine("Unesite brojeve niza:");
+
+            while (broj != 0)
+            {
+                broj = int.Parse(Console.ReadLine());
+                arr.Add(broj);
+            }
+
+            arr.Sort();
+
+            Console.WriteLine("Ispis: ");
+
+            foreach (int a in arr)
+            {
+                Console.WriteLine(a);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
