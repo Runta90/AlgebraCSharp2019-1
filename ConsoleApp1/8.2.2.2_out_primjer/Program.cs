cc9e8b8ea0ce03a9c50a3e4538dcb0205bb4632a
Runta90 <ivan.runtic90@gmail.com>
2019-03-12 16:10:16 +0100

    primjer 8.2.2.2 out

---
+++ ConsoleApp1/8.2.2.2_out_primjer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8._2._2._2_out_primjer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Inicijaliziraj(out a);
+            Console.WriteLine("a = {0}", a);
+
+            Console.ReadKey();
+        }
+
+        private static void Inicijaliziraj(out int a)
+        {
+            a = 1;
+        }
+    }
+}
