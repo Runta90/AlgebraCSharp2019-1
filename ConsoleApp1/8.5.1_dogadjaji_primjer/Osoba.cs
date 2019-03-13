6bb62ce259e5d384e13b7bd1177d0f3481559d14
Runta90 <ivan.runtic90@gmail.com>
2019-03-13 16:57:35 +0100

    8.5.1 dogadjaji primjer

---
+++ ConsoleApp1/8.5.1_dogadjaji_primjer/Osoba.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace primjer_8._5._1
+{
+    internal class Osoba
+    {
+        public delegate void NaPromjenuImenaDelegat(object sender, EventArgs e);
+        public event NaPromjenuImenaDelegat NaPromjenuImena;
+
+        private string ime;
+        public string Ime
+        {
+            get { return ime; }
+            set
+            {
+                ime = value;
+                if (NaPromjenuImena != null)
+                {
+                    NaPromjenuImena(this, new EventArgs());
+                }
+            }
+        }
+    }
+}
