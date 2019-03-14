d520bb8f842f08872876286c5d49b44476dca1e3
Runta90 <ivan.runtic90@gmail.com>
2019-03-14 17:55:51 +0100

    10.1.2 zadatak 3-7

---
+++ ConsoleApp1/10.1.2_3-7_zadatak/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._1._2_3_7_zadatak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            FileStream fs1 = new FileStream("3or7uNoviRed.txt", FileMode.Create);
+            FileStream fs2 = new FileStream("3or7SaZarezima.txt", FileMode.Create);
+
+            StreamWriter sw1 = new StreamWriter(fs1);
+            StreamWriter sw2 = new StreamWriter(fs2);
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 || i % 7 == 0)
+                {
+                    sw1.WriteLine(i);
+                    sw2.Write("{0}, ",i);
+                }
+            }
+
+            sw1.Flush();
+            sw2.Flush();
+
+            sw1.Close();
+            sw2.Close();
+
+            fs1.Close();
+            fs2.Close();
+
+            Console.ReadKey();
+        }
+    }
+}
