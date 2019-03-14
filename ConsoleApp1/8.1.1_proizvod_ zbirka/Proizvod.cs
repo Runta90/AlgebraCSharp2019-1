41a6b9087a7ac7bf19cc29061cd213d7adf9874c
Runtić Ivan <runta_osk@hotmail.com>
2019-03-14 12:47:40 +0100

    vjezba 8.1.1 proizvod

---
+++ ConsoleApp1/8.1.1_proizvod_ zbirka/Proizvod.cs
@@ -0,0 +1,21 @@
+﻿namespace _8._1._1_proizvod__zbirka
+{
+    internal class Proizvod
+    {
+        public string Naziv = "";
+        public double Cijena = 0;
+        public double Marza = 0;
+        public double Porez = 0;
+
+
+        public Proizvod(string naziv)
+        {
+            this.Naziv = naziv;
+        }
+        public double MPC()
+        {
+            return Cijena + Marza + Porez;
+        }
+
+    }
+}
